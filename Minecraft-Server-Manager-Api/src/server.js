9778ca220aaca757990214cf576dab19800aaeed
Alvin Custodio <abydmcustodio09@gmail.com>
2026-03-10 20:42:20 -0400

    Feat: Implementacion del servidor socket.io, error handler, manejo cors

--- Minecraft-Server-Manager-Api/src/server.js
+++ Minecraft-Server-Manager-Api/src/server.js
@@ -1,8 +1,27 @@
-import app from "./index.js"
+import { createServer } from "http";
+import { Server } from "socket.io";
+import app from "./index.js"; 
+import { handleSocketEvents } from "./services/socket-handler-services.js";
 
-const port = process.env.PORT || 3000
+
+const httpServer = createServer(app);
 
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
-})
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    },
+    transports: ['websocket', 'polling']
+});
+
+
+handleSocketEvents(io);
+
+
+const PORT = process.env.SOCKET_PORT || 3000;
+
+
+httpServer.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server socket.io running on port: ${PORT}`);
+});
