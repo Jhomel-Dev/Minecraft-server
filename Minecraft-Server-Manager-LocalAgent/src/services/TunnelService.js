dc592f603bfca9b33508a254c056eb1041c28315
Jhomel <jhomelmedina2@gmail.com>
2026-06-10 00:31:22 -0400

    feat: add playit tunnel service

---
+++ Minecraft-Server-Manager-LocalAgent/src/services/TunnelService.js
@@ -0,0 +1,69 @@
+const { spawn } = require('child_process');
+const EventEmitter = require('events');
+
+class TunnelService extends EventEmitter {
+  constructor() {
+    super();
+    this.playitProcess = null;
+  }
+
+  startTunnel(secretKey = null) {
+    this.verifyNotRunning();
+
+    const args = this.buildArgs(secretKey);
+    this.playitProcess = spawn('playit', args);
+
+    this.attachProcessListeners();
+  }
+
+  stopTunnel() {
+    if (!this.playitProcess) return;
+    this.playitProcess.kill('SIGINT');
+    this.playitProcess = null;
+  }
+
+  verifyNotRunning() {
+    if (this.playitProcess) throw new Error('Tunnel is already running');
+  }
+
+  buildArgs(secretKey) {
+    const args = [];
+    if (secretKey) args.push('--secret', secretKey);
+    return args;
+  }
+
+  attachProcessListeners() {
+    this.playitProcess.stdout.on('data', (data) => this.processOutput(data.toString()));
+    this.playitProcess.stderr.on('data', (data) => this.processError(data.toString()));
+    this.playitProcess.on('close', (code) => this.handleClose(code));
+  }
+
+  processOutput(output) {
+    this.emit('log', output);
+    this.checkForClaimLink(output);
+    this.checkForAssignedAddress(output);
+  }
+
+  processError(error) {
+    this.emit('error', error);
+  }
+
+  handleClose(code) {
+    this.emit('close', code);
+    this.playitProcess = null;
+  }
+
+  checkForClaimLink(output) {
+    const claimLinkRegex = /(https:\/\/playit\.gg\/claim\/[a-zA-Z0-9]+)/;
+    const match = output.match(claimLinkRegex);
+    if (match) this.emit('claim_link', match[1]);
+  }
+
+  checkForAssignedAddress(output) {
+    const addressRegex = /Tunnel address:\s+([a-zA-Z0-9.-]+:[0-9]+)/;
+    const match = output.match(addressRegex);
+    if (match) this.emit('address_assigned', match[1]);
+  }
+}
+
+module.exports = TunnelService;
