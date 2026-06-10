00d5e1b95629104a684af2a532dba3b6c6e93630
Jhomel <jhomelmedina2@gmail.com>
2026-06-10 00:23:33 -0400

    feat: add docker service for minecraft container management

---
+++ Minecraft-Server-Manager-LocalAgent/src/services/DockerService.js
@@ -0,0 +1,65 @@
+const Docker = require('dockerode');
+
+class DockerService {
+  constructor() {
+    this.docker = new Docker();
+  }
+
+  async startMinecraftServer(config) {
+    this.validateConfig(config);
+    await this.pullServerImage();
+    const container = await this.createContainer(config);
+    await container.start();
+    return container.id;
+  }
+
+  validateConfig(config) {
+    if (!config) throw new Error('Missing server configuration');
+    if (!config.name) throw new Error('Missing server name');
+    if (!config.port) throw new Error('Missing server port');
+    if (!config.dataDir) throw new Error('Missing data directory');
+  }
+
+  async pullServerImage() {
+    return new Promise((resolve, reject) => {
+      this.docker.pull('itzg/minecraft-server', (error, stream) => {
+        if (error) return reject(error);
+        
+        this.docker.modem.followProgress(stream, onFinished);
+
+        function onFinished(err, output) {
+          if (err) return reject(err);
+          resolve(output);
+        }
+      });
+    });
+  }
+
+  async createContainer(config) {
+    const options = this.buildContainerOptions(config);
+    return this.docker.createContainer(options);
+  }
+
+  buildContainerOptions(config) {
+    return {
+      Image: 'itzg/minecraft-server',
+      name: `minecraft-${config.name}`,
+      Env: [
+        'EULA=TRUE',
+        `TYPE=${config.type || 'VANILLA'}`,
+        `VERSION=${config.version || 'LATEST'}`,
+        `MEMORY=${config.memory || '2G'}`
+      ],
+      HostConfig: {
+        PortBindings: {
+          '25565/tcp': [{ HostPort: config.port.toString() }]
+        },
+        Binds: [
+          `${config.dataDir}:/data`
+        ]
+      }
+    };
+  }
+}
+
+module.exports = DockerService;
