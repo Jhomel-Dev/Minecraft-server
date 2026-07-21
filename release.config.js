8e8db75155af723334abd7b7fcfa5d0b9e93394d
Jhomel <jhomelmedina2@gmail.com>
2026-07-20 22:13:43 -0400

    fix(release): include AgentGUI and Tauri files in automated versioning

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
       npmPublish: false,
     }],
     ['@semantic-release/exec', {
-      prepareCmd: 'cd Minecraft-Server-Manager-Api && npm version ${nextRelease.version} --no-git-tag-version && cd ../Minecraft-Server-Manager-Web && npm version ${nextRelease.version} --no-git-tag-version && cd ../Minecraft-Server-Manager-LocalAgent && npm version ${nextRelease.version} --no-git-tag-version'
+      prepareCmd: 'node scripts/update-versions.js ${nextRelease.version}'
     }],
     ['@semantic-release/git', {
       assets: [
@@ -20,7 +20,11 @@ module.exports = {
         'Minecraft-Server-Manager-Web/package.json',
         'Minecraft-Server-Manager-Web/package-lock.json',
         'Minecraft-Server-Manager-LocalAgent/package.json',
-        'Minecraft-Server-Manager-LocalAgent/package-lock.json'
+        'Minecraft-Server-Manager-LocalAgent/package-lock.json',
+        'Minecraft-Server-Manager-AgentGUI/package.json',
+        'Minecraft-Server-Manager-AgentGUI/package-lock.json',
+        'Minecraft-Server-Manager-AgentGUI/src-tauri/tauri.conf.json',
+        'Minecraft-Server-Manager-AgentGUI/src-tauri/Cargo.toml'
       ],
       message: 'chore(release): set version to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }],
