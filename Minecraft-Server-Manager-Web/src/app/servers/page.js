e0376bf01734782427a1b43ff46638e78d9c9fd9
Jhomel <jhomelmedina2@gmail.com>
2026-07-23 03:08:04 -0400

    fix: corregir salto de linea en className

--- Minecraft-Server-Manager-Web/src/app/servers/page.js
+++ Minecraft-Server-Manager-Web/src/app/servers/page.js
@@ -145,8 +145,7 @@ function Header({ hideLinkButton, isLinked, agentStatus, onStatusChange, onUnlin
                   <Button 
                     data-cy={agentStatus === 'OFFLINE' ? 'agent-disconnected-btn' : 'agent-connected-btn'}
                     variant="outline" 
-                    className={`w-full sm:w-auto transition-colors ${agentStatus === 'OFFLINE' ? 'border-red-500 text-red-500 hover:bg-red-500/10' : 'border-green-500 text-green-500 hover:bg-green-
-  500/10'}`} 
+                    className={`w-full sm:w-auto transition-colors ${agentStatus === 'OFFLINE' ? 'border-red-500 text-red-500 hover:bg-red-500/10' : 'border-green-500 text-green-500 hover:bg-green-500/10'}`}
                     onClick={() => setShowDropdown(!showDropdown)}
     		 >
                     <HardDrive className="w-5 h-5 mr-2 inline-block" /> 
